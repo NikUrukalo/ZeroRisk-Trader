3455918411f1d2ebfadd398d2be4b7d50a5cc760
Nejc <nz71277@student.uni-lj.si>
2026-08-15 17:12:18 +0200

    Database change

--- Data/create_database.sql
+++ Data/create_database.sql
@@ -1,11 +1,4 @@
-/* Permissions */
-
-GRANT ALL ON DATABASE sem2026_nejczi TO nikuru WITH GRANT OPTION;
-GRANT CONNECT ON DATABASE sem2026_nejczi TO nikuru;
-GRANT CONNECT ON DATABASE sem2026_nejczi TO javnost;
-
-
-/* Table Creation */
+/* ======================== Table Creation =========================== */
 
 CREATE TABLE IF NOT EXISTS app_user (
     user_id SERIAL PRIMARY KEY,
@@ -22,35 +15,62 @@ CREATE TABLE IF NOT EXISTS portfolio (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS asset_master (
+    asset_symbol TEXT PRIMARY KEY,
+    asset_name TEXT NOT NULL,
+    asset_type TEXT NOT NULL -- e.g., stock, ETF, cryptocurrency
+);
+
 CREATE TABLE IF NOT EXISTS asset (
     asset_id SERIAL PRIMARY KEY,
-    asset_name TEXT NOT NULL,
-    asset_symbol TEXT NOT NULL,
-    asset_type TEXT NOT NULL, -- e.g., stock, bond, cryptocurrency
+    asset_symbol TEXT NOT NULL REFERENCES asset_master(asset_symbol) ON DELETE CASCADE,
     price NUMERIC(10, 2) NOT NULL,
     date_stamp DATE NOT NULL,
     time_stamp TIME NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS trade (
-	trade_id SERIAL PRIMARY KEY,
-	portfolio_id INTEGER NOT NULL REFERENCES portfolio(portfolio_id) ON DELETE CASCADE,
-	asset_symbol TEXT NOT NULL REFERENCES asset(asset_symbol) ON DELETE CASCADE,
-	trade_type TEXT NOT NULL CHECK (trade_type in ('BUY', 'SELL')),
-	quantity NUMERIC(10, 4) NOT NULL CHECK (quantity > 0),
-	price NUMERIC(10, 2) NOT NULL,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    trade_id SERIAL PRIMARY KEY,
+    portfolio_id INTEGER NOT NULL REFERENCES portfolio(portfolio_id) ON DELETE CASCADE,
+    asset_symbol TEXT NOT NULL REFERENCES asset_master(asset_symbol) ON DELETE CASCADE,
+    trade_type TEXT NOT NULL CHECK (trade_type IN ('BUY', 'SELL')),
+    quantity NUMERIC(10, 4) NOT NULL CHECK (quantity > 0),
+    price NUMERIC(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS position (
-	position_id SERIAL PRIMARY KEY,
-	portfolio_id INTEGER NOT NULL REFERENCES portfolio(portfolio_id) ON DELETE CASCADE,
-	asset_symbol TEXT NOT NULL REFERENCES asset(asset_symbol) ON DELETE CASCADE,
-	quantity NUMERIC(10, 4) NOT NULL CHECK (quantity > 0),
-	avg_buy_price NUMERIC(10, 2) NOT NULL,
-	UNIQUE (portfolio_id, asset_id) -- so that we have all assets in a portfolio together
+    position_id SERIAL PRIMARY KEY,
+    portfolio_id INTEGER NOT NULL REFERENCES portfolio(portfolio_id) ON DELETE CASCADE,
+    asset_symbol TEXT NOT NULL REFERENCES asset_master(asset_symbol) ON DELETE CASCADE,
+    quantity NUMERIC(10, 4) NOT NULL CHECK (quantity > 0),
+    avg_buy_price NUMERIC(10, 2) NOT NULL,
+    UNIQUE (portfolio_id, asset_symbol) -- one position per asset per portfolio
 );
 
-SELECT *
-FROM POSITION
 
+
+/* ======================== Permissions =========================== */
+
+GRANT ALL ON DATABASE sem2026_nejczi TO nikuru WITH GRANT OPTION;
+GRANT CONNECT ON DATABASE sem2026_nejczi TO nikuru;
+GRANT CONNECT ON DATABASE sem2026_nejczi TO javnost;
+
+GRANT SELECT, DELETE, INSERT, UPDATE ON TABLE app_user TO javnost;
+GRANT ALL PRIVILEGES ON SEQUENCE app_user_user_id_seq TO javnost;
+
+GRANT SELECT, DELETE, INSERT, UPDATE ON TABLE portfolio TO javnost;
+GRANT ALL PRIVILEGES ON SEQUENCE portfolio_portfolio_id_seq TO javnost;
+
+GRANT SELECT ON TABLE asset_master TO javnost;
+GRANT SELECT, DELETE, INSERT, UPDATE ON TABLE asset_master TO nikuru;
+
+GRANT SELECT ON TABLE asset TO javnost;
+GRANT SELECT, DELETE, INSERT, UPDATE ON TABLE asset TO nikuru;
+GRANT ALL PRIVILEGES ON SEQUENCE asset_asset_id_seq TO nikuru;
+
+GRANT SELECT, DELETE, INSERT, UPDATE ON TABLE trade TO javnost;
+GRANT ALL PRIVILEGES ON SEQUENCE trade_trade_id_seq TO javnost;
+
+GRANT SELECT, DELETE, INSERT, UPDATE ON TABLE position TO javnost;
+GRANT ALL PRIVILEGES ON SEQUENCE position_position_id_seq TO javnost;
