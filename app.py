29f80fdd76341fe9fbd7c14eb2c9660e17edd058
NikUrukalo <nik.urukalo@gmail.com>
2026-08-16 15:14:43 +0200

    Added first html

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from bottle import Bottle, run, template
+import bottle
+
+bottle.TEMPLATE_PATH.insert(0, 'Presentation/views/')
 
 app = Bottle()
 
+@app.route('/')
 @app.route('/login')
 def login():
-    return template('Presentation/views/login', napaka=None)
+    return template('login', error=None)
 
 if __name__ == '__main__':
     run(app, host='localhost', port=8080, debug=True, reloader=True)
