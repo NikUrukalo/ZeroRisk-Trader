966bf427aa14fa4a0ac375ca7eeb9cd0e5bcead2
Nejc <nz71277@student.uni-lj.si>
2026-08-16 14:25:06 +0200

    Html first try

--- app.py
+++ app.py
@@ -1,27 +1,10 @@
-from functools import wraps
-from Presentation.bottleext import get, post, run, request, template, redirect, static_file, url, response, template_user
+from bottle import Bottle, run, template
 
-from Services.transakcije_service import TransakcijeService
-from Services.auth_service import AuthService
-import os
+app = Bottle()
 
-# Ustvarimo instance servisov, ki jih potrebujemo. 
-# Če je število servisov veliko, potem je service bolj smiselno inicializirati v metodi in na
-# začetku datoteke (saj ne rabimo vseh servisov v vseh metodah!)
+@app.route('/login')
+def login():
+    return template('Presentation/views/login', napaka=None)
 
-service = TransakcijeService()
-auth = AuthService()
-
-
-# privzete nastavitve
-SERVER_PORT = os.environ.get('BOTTLE_PORT', 8080)
-RELOADER = os.environ.get('BOTTLE_RELOADER', True)
-
-
-
-
- # Dokler nimate razvitega vmesnika za dodajanje uporabnikov, jih dodajte kar ročno.
-#auth.dodaj_uporabnika('gasper', 'admin', 'gasper')
-if __name__ == "__main__":
-   
-    run(host='localhost', port=SERVER_PORT, reloader=RELOADER, debug=True)
+if __name__ == '__main__':
+    run(app, host='localhost', port=8080, debug=True, reloader=True)
