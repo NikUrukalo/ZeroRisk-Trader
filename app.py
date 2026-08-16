ad9dbeb7ccf8240ea2aeb64a58b5ff879a7948ee
Nejc <nz71277@student.uni-lj.si>
2026-08-16 16:05:14 +0200

    first page

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from bottle import Bottle, run, template
 import bottle
+from bottle import Bottle, run, template, static_file
 
 bottle.TEMPLATE_PATH.insert(0, 'Presentation/views/')
 
 app = Bottle()
 
+# Serve static CSS / JS
+@app.route('/static/<filename:path>')
+def serve_static(filename):
+    return static_file(filename, root='Presentation/public/')
+
 @app.route('/')
-@app.route('/login')
+@app.route('/login', name='login')
 def login():
-    return template('login', error=None)
+    return template('login', user=None, success=None, error=None)
 
 if __name__ == '__main__':
     run(app, host='localhost', port=8080, debug=True, reloader=True)
